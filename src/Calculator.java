11e5139e805cb9e113001915c636381cd928d1f5
jpratt-mru <jpratt@mtroyal.ca>
2020-02-12 16:33:36 +0000

    Add exception catch

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,23 +2,24 @@
 
 public class Calculator {
 
-	public Calculator() {
+    public Calculator() {
+    }
+    /**
+     * Receives two double values and performs an action. If a is less than b, a*b, otherwise, a/b will be returned.
+     * @param a
+     * @param b
+     * @return the result of the action performed on and b.
+     * @throws ArithmeticException is b is 0;
+     */
+    public double action(double a, double b) {
+	if (b == 0) throw new ArithmeticException("boom");
+	double result = 0;
+	if ( a < b) {
+	    result = a * b;
+	}else {
+	    result = a/b;
 	}
-	/**
-	 * Receives two double values and performs an action. If a is less than b, a*b, otherwise, a/b will be returned.
-	 * @param a
-	 * @param b
-	 * @return the result of the action performed on and b.
-	 * @throws ArithmeticException is b is 0;
-	 */
-	public double action(double a, double b) {
-		double result = 0;
-		if (a<b) {
-			result = a * b;
-		}else {
-			result = a/b;
-		}
-		return result;
-	}
+	return result;
+    }
 
 }
